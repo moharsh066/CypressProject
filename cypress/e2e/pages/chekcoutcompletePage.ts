22587be990509272ac1717120508750345a85f46
moharsh066 <moharsh066@gmail.com>
2024-09-07 07:23:52 +0200

    completePageChanges

--- cypress/e2e/pages/chekcoutcompletePage.ts
+++ cypress/e2e/pages/chekcoutcompletePage.ts
@@ -1,12 +1,47 @@
+import { sys } from "typescript";
 
 
-export class CheckoutcompletePage{
- purchaseconfirmation_text=  '[data-test="complete-header"]'
+export class CheckoutcompletePage {
+   purchaseconfirmation_text = '[data-test="complete-header"]'
 
 
- validateConfirmation(){
-    cy.get(this.purchaseconfirmation_text).should('contain', 'Thank you for your order!')
- }
+
+   validateConfirmation() {
+      //cy.get(this.purchaseconfirmation_text).should('have.text', 'Thank you for your order!')
+
+      cy.get(this.purchaseconfirmation_text).then((element) => {
+         const confirmationText = element.text().trim();
+
+         switch (confirmationText) {
+            case "Thank you for your order!":
+
+               cy.get(this.purchaseconfirmation_text).should('have.text', 'Thank you for your order!')
+               cy.log(("Message verified in English"))
+               break;
+
+            case "Merci pour votre commande !":
+
+               cy.get(this.purchaseconfirmation_text).should('have.text', 'Merci pour votre commande !')
+               cy.log(("Message verified in French"))
+               break;
+
+            case "¡Gracias por tu pedido!":
+
+               cy.get(this.purchaseconfirmation_text).should('have.text', '¡Gracias por tu pedido!')
+               cy.log(("Message verified in Spanish"))
+               break;
+
+            case "Grazie per il tuo ordine!":
+
+               cy.get(this.purchaseconfirmation_text).should('have.text', 'Grazie per il tuo ordine!')
+               cy.log(("Message verified in Italian"))
+               break;
+
+            default:
+               console.error("Confirmation text did not match any expected values.")
+         }
+      });
+   }
+
 }
-
 export default CheckoutcompletePage;
